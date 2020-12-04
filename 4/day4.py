dc3e09019449507479aa9e7d07082f22e732a6c3
Olof Zetterlund <2705677+ozetterlund@users.noreply.github.com>
2020-12-04 15:48:20 +0100

    Refined solution with lamnda functions

--- 4/day4.py
+++ 4/day4.py
@@ -1,61 +1,25 @@
 import re
 
-inputList = []
+passports = []
 with open("input.txt") as f:
-    inputList = f.read().split('\n\n')
-    for i in range(len(inputList)):
-        inputList[i] = inputList[i].replace('\n',' ').split(' ')
+    passports = f.read().split('\n\n')
 
-inputDictList = []
-for input in inputList:
-    inputDict = {}
-    for entry in input:
-        key, val = entry.split(':')
-        inputDict[key] = val
-        
-    inputDictList.append(inputDict)
+p1 = p2 = 0
+fields = {'iyr': lambda x: 2010 <= int(x) <= 2020,
+          'eyr': lambda x: 2020 <= int(x) <= 2030,
+          'hcl': lambda x: bool(re.search("^#[0-9a-f]{6}$", x)),
+          'byr': lambda x: 1920 <= int(x) <= 2002,
+          'ecl': lambda x: x in ['amb','blu','brn','gry','grn','hzl','oth'],
+          'hgt': lambda x: bool(re.search("(^1([5-8][0-9]|9[0-3])cm$)|(^(59|6[0-9]|7[0-6])in$)", x)),
+          'pid': lambda x: bool(re.search("^[0-9]{9}$", x))}
 
-# Part 1
+for passport in passports:
+    pDict = dict(x.split(':') for x in passport.replace('\n', ' ').split(' '))
+    if all([x in pDict for x in fields.keys()]):
+        p1 += 1
+        pDict.pop('cid', None)
+        if all([fields[x](pDict[x]) for x in pDict]):
+            p2 += 1
 
-validPassports = 0
-
-for passport in inputDictList:
-    if len(passport) == 8 or (len(passport) == 7 and 'cid' not in passport):
-        validPassports += 1
-
-print(f"Part one: {validPassports}")
-
-# Part 2
-
-def checkbyr(year):
-    return 1920 <= int(year) <= 2002
-def checkiyr(year):
-    return 2010 <= int(year) <= 2020
-def checkeyr(year):
-    return 2020 <= int(year) <= 2030
-def checkhgt(hgt):
-    x = re.search("(^1([5-8][0-9]|9[0-3])cm$)|(^(59|6[0-9]|7[0-6])in$)", hgt)
-    return bool(x)
-def checkhcl(hcl):
-    x = re.search("^#[0-9a-f]{6}$", hcl)
-    return bool(x)
-def checkecl(color):
-    return color in ['amb','blu','brn','gry','grn','hzl','oth']
-def checkpid(pid):
-    x = re.search("^[0-9]{9}$", pid)
-    return bool(x)
-    
-validPassports = 0
-for passport in inputDictList:
-    if len(passport) == 8 or (len(passport) == 7 and 'cid' not in passport):
-        if (checkbyr(passport['byr']) and 
-        checkiyr(passport['iyr']) and 
-        checkeyr(passport['eyr']) and 
-        checkhgt(passport['hgt']) and 
-        checkhcl(passport['hcl']) and
-        checkecl(passport['ecl']) and
-        checkpid(passport['pid'])):
-            validPassports += 1
-
-print(f"Part two: {validPassports}")
-
+print(f"Part one: {p1}")
+print(f"Part two: {p2}")
